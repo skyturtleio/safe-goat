81387030fc04227a2befc0ac09be58e4a3e0cc39
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-29 21:27:01 -0700

    add counter

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+
+	function decrement() {
+		count -= 1;
+	}
+
+	function reset() {
+		count = 0;
+	}
+</script>
+
+<h1>Counter</h1>
+
+<h2>{count}</h2>
+<button on:click={decrement}>-</button>
+<button on:click={reset}>reset</button>
+<button on:click={increment}>+</button>
